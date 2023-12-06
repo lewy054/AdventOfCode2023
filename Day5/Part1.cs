59a9a47e5d73da3f55353e7601766b50b421b4a2
lewy054 <lewy054@gmail.com>
2023-12-07 00:48:00 +0100

    Add day5 part1

--- Day5/Part1.cs
+++ Day5/Part1.cs
@@ -3,7 +3,7 @@ using Helpers;
 
 namespace Day5;
 
-public class Part1 : IResolver
+public class Part1
 {
     public Part1(IList<string> input)
     {
@@ -12,16 +12,23 @@ public class Part1 : IResolver
 
     public IList<string> Input { get; init; }
 
-    public int GetResult()
+    public long GetResult()
     {
         var almanac = new Almanac(Input.ToList());
+        var locations = new List<long>();
         foreach (var seed in almanac.Seeds)
         {
-            // var soil = 
+            var soil = almanac.SeedToSoil.GetDestinationValue(seed);
+            var fertilizer = almanac.SoilToFertilizer.GetDestinationValue(soil);
+            var water = almanac.FertilizerToWater.GetDestinationValue(fertilizer);
+            var light = almanac.WaterToLight.GetDestinationValue(water);
+            var temperature = almanac.LightToTemperature.GetDestinationValue(light);
+            var humidity = almanac.TemperatureToHumidity.GetDestinationValue(temperature);
+            var location = almanac.HumidityToLocation.GetDestinationValue(humidity);
+            locations.Add(location);
         }
-        return 0;
+
+        var minLocation = locations.Min();
+        return minLocation;
     }
-    
-    // private 
-    
 }
