40422204e0a705bc2a1230c25ba58d7c13b29856
lewy054 <norbert.lewicz1@gmail.com>
2023-12-08 17:08:52 +0100

    Add day6

---
+++ Day6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+using Day6;
+using Helpers;
+
+const string inputFileName = "input.txt";
+var input = FileHelpers.GetFileContent(inputFileName).ToList();
+
+var times = Regex.Matches(input.ElementAt(0), @"\d+").Select(e => int.Parse(e.Value)).ToList();
+var distances = Regex.Matches(input.ElementAt(1), @"\d+").Select(e => int.Parse(e.Value)).ToList();
+
+var result = 1;
+for (var i = 0; i < times.Count; i++)
+{
+    var race = new Race(times[i], distances[i]);
+    result *= race.GetNumberOfPossibleWaysToWin();
+}
+
+Console.WriteLine($"Result of Day6, Part1: {result}");
+
+var part2Time = long.Parse(string.Concat(times));
+var part2Distance = long.Parse(string.Concat(distances));
+
+var race2 = new Race(part2Time, part2Distance);
+var part2Result = race2.GetNumberOfPossibleWaysToWin();
+
+
+Console.WriteLine($"Result of Day6, Part1: {part2Result}");
---
+++ Day6/Race.cs
@@ -0,0 +1,30 @@
+﻿namespace Day6;
+
+public class Race
+{
+    public Race(long duration, long record)
+    {
+        Duration = duration;
+        Record = record;
+    }
+
+    private long Record { get; set; }
+    private long Duration { get; set; }
+
+
+    public int GetNumberOfPossibleWaysToWin()
+    {
+        var waysToWin = 0;
+        for (var j = 1; j < Duration; j++) // time to hold button
+        {
+            var timeToTravel = Duration - j;
+            var score = timeToTravel * j;
+            if (score > Record)
+            {
+                waysToWin += 1;
+            }
+        }
+
+        return waysToWin;
+    }
+}
