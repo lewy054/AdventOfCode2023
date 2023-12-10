4d9502b8c9d264943e304ae5c16ba347aa7ba33e
lewy054 <lewy054@gmail.com>
2023-12-10 11:37:34 +0100

    Add day9 part1

---
+++ Day9/Part1.cs
@@ -0,0 +1,60 @@
+﻿namespace Day9;
+
+public class Part1
+{
+    public Part1(List<string> input)
+    {
+        Input = input;
+    }
+
+    private List<string> Input { get; }
+
+    public int GetResult()
+    {
+        var extrapolatedHistoriesSum = 0;
+        foreach (var history in Input)
+        {
+            var histories = new List<List<int>>();
+            var values = history.Split(" ").Select(int.Parse).ToList();
+            histories.Add(values);
+            PredictNextValue(ref histories);
+            var extrapolatedHistories = ExtrapolateHistory(histories);
+            extrapolatedHistoriesSum += extrapolatedHistories.First().Last();
+        }
+
+
+        return extrapolatedHistoriesSum;
+    }
+
+    private static List<List<int>> PredictNextValue(ref List<List<int>> histories)
+    {
+        var lineToExtrapolate = histories.Last();
+        if (lineToExtrapolate.All(e => e == 0))
+        {
+            return histories;
+        }
+
+        var nextLine = new List<int>();
+        for (var i = 1; i < lineToExtrapolate.Count; i++)
+        {
+            var value = lineToExtrapolate[i] - lineToExtrapolate[i - 1];
+            nextLine.Add(value);
+        }
+
+        histories.Add(nextLine);
+        return PredictNextValue(ref histories);
+    }
+
+    private static List<List<int>> ExtrapolateHistory(List<List<int>> history)
+    {
+        history.Last().Add(0);
+        for (var i = history.Count - 2; i >= 0; i--)
+        {
+            var sameRowLast = history[i].Last();
+            var previousRowLast = history[i + 1].Last();
+            history[i].Add(sameRowLast + previousRowLast);
+        }
+
+        return history;
+    }
+}
---
+++ Day9/Program.cs
@@ -0,0 +1,9 @@
+﻿using Day9;
+using Helpers;
+
+const string fileName = "input.txt";
+var input = FileHelpers.GetFileContent(fileName).ToList();
+var part1 = new Part1(input);
+var part1Result = part1.GetResult();
+
+Console.WriteLine($"Day 9, Part1 result: {part1Result}");
